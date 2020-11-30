435e297a070fc3bb03ae75a1ae97b8a771859027
Rudolf Peralta <Rudolf Peralta@LAPTOP-6A8GJ9L1>
2020-11-30 15:34:59 +0800

    My first commit

---
+++ HelloWorldProject/src/HellowWorldPackage/helloWorld.java
@@ -0,0 +1,21 @@
+package HellowWorldPackage;
+
+public class helloWorld {
+	
+	static String name = new String();
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		//System.out.print("Mabuhay Pilipinas");
+		name = "rudolf";
+		//name.toUpperCase();
+		System.out.print("Mabuhay Pilipinas " + name.toUpperCase());
+		
+		
+		
+		
+		
+
+	}
+
+}
