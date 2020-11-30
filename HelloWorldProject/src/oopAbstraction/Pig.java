391bb87020add7642bbea2de3581c99ae8927775
Rudolf Peralta <Rudolf Peralta@LAPTOP-6A8GJ9L1>
2020-11-30 23:23:55 +0800

    added println

--- HelloWorldProject/src/oopAbstraction/Pig.java
+++ HelloWorldProject/src/oopAbstraction/Pig.java
@@ -4,6 +4,7 @@ public class Pig extends Animal{
 	  public void animalSound() {
 		    // The body of animalSound() is provided here
 		    System.out.println("The pig says: wee wee");
+		    System.out.println("Pipa pig says: onk onk");
 		  }
 
 }
