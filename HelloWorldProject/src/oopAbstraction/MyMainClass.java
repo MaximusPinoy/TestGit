df8105f242d267683b1913aaafac9cef3550c1e3
Rudolf Peralta <Rudolf Peralta@LAPTOP-6A8GJ9L1>
2020-11-30 23:22:25 +0800

    Abstraction

---
+++ HelloWorldProject/src/oopAbstraction/MyMainClass.java
@@ -0,0 +1,12 @@
+package oopAbstraction;
+
+public class MyMainClass {
+
+	public static void main(String[] args) {
+	    Pig myPig = new Pig(); // Create a Pig object
+	    myPig.animalSound();
+	    myPig.sleep();
+
+	}
+
+}
